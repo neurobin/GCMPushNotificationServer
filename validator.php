1b5218c95a0caa92b4599d44226e22bc7fdefeff
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-05-03 08:05:17 +0600

    init

---
+++ validator.php
@@ -0,0 +1,45 @@
+<?php
+class Validator{
+
+function email($email){
+
+}
+
+function username($username){
+
+}
+function name($name){
+
+}
+
+function password($password){
+
+}
+
+function text($text){
+
+}
+
+function plaintext($plaintext){
+
+}
+
+function int($int){
+
+}
+function float($float){
+
+}
+function pureFloat($pureFloat){
+
+}
+function scientific($scientific){
+
+}
+function string($string){
+
+}
+
+}
+
+?>
